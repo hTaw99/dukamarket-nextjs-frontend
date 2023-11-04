5b3255b4af0d0f9e449028ac7169148e80dc1457
hTaw99 <hady.tawfik1999@gmail.com>
2023-11-04 21:52:14 +0200

    remove async

--- src/actions/revalidateAction.js
+++ src/actions/revalidateAction.js
@@ -3,7 +3,7 @@
 import { revalidatePath, revalidateTag } from "next/cache";
 import { redirect } from "next/navigation";
 
-export const revalidateAction = async (id) => {
+export const revalidateAction = (id) => {
   // console.log(id);
   // revalidatePath(`/products/${id}`);
   revalidateTag("singleProduct");
